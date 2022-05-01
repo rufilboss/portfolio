51b2fd11a61c5c3ab8d987946132e35ea438d5c3
rufilboy <rufaiilyas600@gmail.com>
2022-05-01 17:57:27 +0100

    Update

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -6,7 +6,7 @@ import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 const data = [
   { number: 3, text: 'Open Source Projects'},
   { number: 10, text: 'Certifications', },
-  { number: 20, text: 'Github Followers', },
+  { number: 10, text: 'Github Followers', },
   { number: 5, text: 'DevOps Projects', }
 ];
 
