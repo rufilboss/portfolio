6c6d3b205f76d450a8eedd2243c2f296d40d6044
Ilyas Rufai <rufaiilyas@gmail.com>
2025-09-15 12:45:03 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,14 +12,25 @@ function Footer() {
   let year = date.getFullYear();
   return (
     <Container fluid className="footer">
-      <Row className="justify-content-center">
-        <Col md="12" className="footer-body text-center">
+      <Row className="align-items-center footer-row">
+        {/* Left: Designed by (mobile order 2) */}
+        <Col xs="12" md="4" className="footer-copywright order-2 order-md-1 text-center text-md-start">
+          <p className="footer-small">Designed and Developed by ILYAS RUFAI</p>
+        </Col>
+
+        {/* Center: Copyright (mobile order 3) */}
+        <Col xs="12" md="4" className="footer-copywright order-3 order-md-2 text-center">
+          <p className="footer-small">Copyright © {year} rufilboss</p>
+        </Col>
+
+        {/* Right: Icons (mobile order 1) */}
+        <Col xs="12" md="4" className="footer-body order-1 order-md-3 text-center text-md-end">
           <ul className="footer-icons footer-icons-large">
             <li className="social-icons">
               <a
                 href="https://github.com/rufilboss"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
@@ -29,7 +40,7 @@ function Footer() {
               <a
                 href="https://twitter.com/rufilboss"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiOutlineTwitter />
@@ -39,7 +50,7 @@ function Footer() {
               <a
                 href="https://www.linkedin.com/in/rufilboss/"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaLinkedinIn />
@@ -49,17 +60,13 @@ function Footer() {
               <a
                 href="https://www.instagram.com/rufilboss"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillInstagram />
               </a>
             </li>
           </ul>
-          <div className="footer-copywright text-center">
-            <p className="footer-small">Designed and Developed by ILYAS RUFAI</p>
-            <p className="footer-small">Copyright © {year} rufilboss</p>
-          </div>
         </Col>
       </Row>
     </Container>
