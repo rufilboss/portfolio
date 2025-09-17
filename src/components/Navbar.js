e172404637517cf90753542cf75893aa41e15520
Ilyas Rufai <rufaiilyas@gmail.com>
2025-09-17 11:47:25 +0100

    fix: mobile view issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 
 const Navigation = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar expand="lg" className="navbar">
+    <Navbar expand="lg" className="navbar" expanded={expanded} onToggle={setExpanded}>
       <div className="container">
-        <Navbar.Brand href="#home" className="navbar-brand">
+        <Navbar.Brand href="#home" className="navbar-brand" onClick={() => setExpanded(false)}>
           <img
             src={process.env.PUBLIC_URL + "/Images/logo.png"}
             alt="Ilyas Rufai Logo"
@@ -15,14 +17,14 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
-            <Nav.Link href="#about" className="nav-link">About</Nav.Link>
-            <Nav.Link href="#career-highlights" className="nav-link">Career Highlights</Nav.Link>
-            <Nav.Link href="#projects" className="nav-link">Projects</Nav.Link>
-            <Nav.Link href="#publications" className="nav-link">Publications</Nav.Link>
-            <Nav.Link href="#academic" className="nav-link">Academic</Nav.Link>
-            <Nav.Link href="#network" className="nav-link">Network</Nav.Link>
-            <Nav.Link href="#contact" className="nav-link">Contact</Nav.Link>
+            <Nav.Link href="#home" className="nav-link" onClick={() => setExpanded(false)}>Home</Nav.Link>
+            <Nav.Link href="#about" className="nav-link" onClick={() => setExpanded(false)}>About</Nav.Link>
+            <Nav.Link href="#career-highlights" className="nav-link" onClick={() => setExpanded(false)}>Career Highlights</Nav.Link>
+            <Nav.Link href="#projects" className="nav-link" onClick={() => setExpanded(false)}>Projects</Nav.Link>
+            <Nav.Link href="#publications" className="nav-link" onClick={() => setExpanded(false)}>Publications</Nav.Link>
+            <Nav.Link href="#academic" className="nav-link" onClick={() => setExpanded(false)}>Academic</Nav.Link>
+            <Nav.Link href="#network" className="nav-link" onClick={() => setExpanded(false)}>Network</Nav.Link>
+            <Nav.Link href="#contact" className="nav-link" onClick={() => setExpanded(false)}>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </div>
